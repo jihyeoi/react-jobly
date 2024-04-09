6a69728ed2cc78f297f8e9dd9f810dd0ee7479c5
andres-ordonezs <andresmw.89@gmail.com>
2024-04-09 19:08:22 -0400

    first commit

---
+++ jobly/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Jobly from "./Jobly";
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ *
+ */
+
+function App() {
+  return (
+    <div className="App">
+      <Jobly />
+    </div>
+  );
+}
+
+export default App;
---
+++ jobly/src/Jobly.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+function Jobly() {
+  return (
+    <div>
+      <h1>Jobly</h1>
+    </div>
+  );
+}
+
+export default Jobly;
